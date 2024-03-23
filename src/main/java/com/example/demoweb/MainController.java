43059f19baa9b699a64f31612c77309780c7e635
mikeqiu <mikeqiu2011@163.com>
2024-03-23 16:41:32 +0800

    request parameter optional

--- src/main/java/com/example/demoweb/MainController.java
+++ src/main/java/com/example/demoweb/MainController.java
@@ -3,13 +3,16 @@ package com.example.demoweb;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     @RequestMapping("/home")
-    public String home(Model page){
-        page.addAttribute("username", "katy");
-        page.addAttribute("color", "red");
+    public String home(Model page,
+                       @RequestParam(required = false) String color,
+                       @RequestParam(required = false) String name){
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
         return "home.html";
     }
 }
